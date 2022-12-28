c75150ef8875c0795af1ca1b95530e9fc740773e
rzamirzayev <118839663+rzamirzayev@users.noreply.github.com>
2022-12-28 19:41:10 +0400

    Update SrudentService.cs

--- SrudentService.cs
+++ SrudentService.cs
@@ -11,10 +11,10 @@ namespace ClassTask
     internal class StudentService : Stundent
     {
         Stundent s = new();
-        
+
         public void Create()
         {
-            
+
             Console.Write("Adinzi daxil edin:");
             s.name = Console.ReadLine();
             Array.Resize(ref Group.Students, Group.Students.Length + 1);
@@ -25,44 +25,49 @@ namespace ClassTask
 
 
             Console.Write("Soyadinizi daxil edin:");
-           s.surname = Console.ReadLine();
+            s.surname = Console.ReadLine();
             Array.Resize(ref Group.Students, Group.Students.Length + 1);
             {
                 Group.Students[Group.Students.Length - 1] = s.surname;
             }
 
             bool controlname = false;
-            bool controlsurname=false;  
-            if(s.name.Length>=3 && s.name.Length<15)
+            bool controlsurname = false;
+            if (s.name.Length >= 3 && s.name.Length < 15)
             {
-                controlname= true;
+                controlname = true;
                 id++;
-               
+
                 if (s.surname.Length >= 3)
                 {
-                    controlsurname= true;
+                    controlsurname = true;
                     id++;
-                 
+
                 }
-                Console.WriteLine("Idiniz:"+id/2);
-                
+                Console.WriteLine("Idiniz:" + id / 2);
+
             }
-     
+
         }
         public void Delete()
         {
             Console.Write("Silmek istediyiniz id qeyd edin:");
             int deleteid = Convert.ToInt32(Console.ReadLine());
             bool idcontrol = false;
-            Group.Students[(deleteid * 2)] = string.Empty;
+            Group.Students[(deleteid * 2)] =string.Empty;
             Group.Students[(deleteid * 2) + 1] = string.Empty;
-   
+
         }
         public void Edit()
         {
             Console.WriteLine("Deyismek istediyiniz idini daxil edin:");
             int editid = Convert.ToInt32(Console.ReadLine());
-       
+            Console.WriteLine("Deyismek istediyinz adi daxil edin.");
+            Group.Students[editid * 2] = Console.ReadLine();
+            Console.WriteLine("Deyismek istediyiniz soyadi daxil edin.");
+            Group.Students[(editid * 2) + 1] = Console.ReadLine();
+
+
 
 
         }
@@ -73,7 +78,7 @@ namespace ClassTask
                 Console.WriteLine(item);
             }
         }
-       
+
 
     }
 }
