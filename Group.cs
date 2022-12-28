09a3beb78917e9374b68ee088629d0ad82b982d6
rzamirzayev <118839663+rzamirzayev@users.noreply.github.com>
2022-12-28 11:11:53 +0400

    Create Group.cs

---
+++ Group.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    internal class Group : Stundent
+    {
+        public  int Id { get; set; } 
+        public  string name { get; set; }
+        public static string[] Students = new string[id];
+        
+        
+       
+        
+       
+        
+    }
+}
