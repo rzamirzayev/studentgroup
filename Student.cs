e8efa843e577579cc7bf21ab371e9fd8b71edf04
rzamirzayev <118839663+rzamirzayev@users.noreply.github.com>
2022-12-28 20:36:54 +0400

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -9,41 +9,45 @@ namespace ClassTask
 {
     internal class Stundent
     {
-        
-        public static int id=0;
+
+        public static int id = 0;
         private string _name;
         private string _surname;
         private int _avarage;
-       
-        public string name { 
-            get { return _name;}
-            set {
-                if(value.Length <3 || value.Length>15) {
+
+        public string name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 3 || value.Length > 15)
+                {
                     Console.WriteLine("name herf sayi 3 ve 15araliginda olmalidir");
                 }
-                _name = value; }
-
+                _name = value;
             }
-        
+
+        }
+
         public string surname
         {
-            get { return _surname;}
+            get { return _surname; }
             set { _surname = value; }
         }
         public int avarage
         {
             get { return _avarage; }
-            set 
+            set
             {
-                if (value < 0 || value>100)
+                if (value < 0 || value > 100)
                     Console.WriteLine("Avarage 0 ve 100 araliginda olmalidir");
                 else
                     _avarage = value;
-          
+
             }
-           
+
         }
-        
+
 
     }
 }
